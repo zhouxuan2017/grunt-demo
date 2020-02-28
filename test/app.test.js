477a72d2787ad2d233bac65412dbadf0f4434cad
zhouxuan <1207469303@qq.com>
2020-02-28 11:44:47 +0800

    http-api-test

--- test/app.test.js
+++ test/app.test.js
@@ -1,8 +1,8 @@
-const expect=require('chai').expect.
+const expect=require('chai').expect
      http=require('http');
 
      describe('矩形计算器HTTP API 接口测试',function(){
-       it('正确请求格式测试',function(){
+       it('正确请求格式测试',function(done){
          http.get('http://localhost:8080/rectangle?width=5&height=7',function(res){
            var result='';
            res.on('data',(chunk)=>{
@@ -12,6 +12,7 @@ const expect=require('chai').expect.
               var rect=JSON.parse(result);
               expect(rect.area).to.be.equal(35);
               expect(rect.perimeter).to.be.equal(24);
+              done();
            })
            })
        })
