c9d13c51cebfad49bdc0ef38f8b8ffce8ff6c160
zhouxuan <1207469303@qq.com>
2020-02-28 08:59:57 +0800

    mocha-fe

---
+++ util.js
@@ -0,0 +1,11 @@
+function rectangle(){
+  return {
+    'perimeter':function(width,height){
+      return 2*(width+height)
+    },
+    'area':function(width,height)
+    {
+      return width*height;
+    }
+  }
+}
