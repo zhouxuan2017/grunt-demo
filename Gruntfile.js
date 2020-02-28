9197010830c76af1077d3d9df4a82ff4eae1d4a6
zhouxuan <1207469303@qq.com>
2020-02-28 12:54:32 +0800

    http-api-test

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,24 @@
 /* global module: true  */
 module.exports = function (grunt) {
   grunt.initConfig({
-    mocha:{
-      test:{
-        src:['test/index.html']
-      },
+    run:{
+      api:{
+        options:{wait:false},
+       args:['./app.js']
+      }
+    },
+    mochacli:{
+      all:['test/*.js'],
   options:{
-    run:true,
-  reporter:'Dot'
+  reportor:'spec',
+  bail:true
   }
     }
     });
 
-  grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks('grunt-mocha-cli');
+  grunt.loadNpmTasks('grunt-run');
 
-  grunt.registerTask('default', ['mocha']);
+  grunt.registerTask('default', ['run','mochacli','stop']);
 };
 
