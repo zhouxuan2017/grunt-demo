ac8d2e8455b65afbc1015a1a77c88e3e4cf62995
zhouxuan <1207469303@qq.com>
2020-03-04 09:04:53 +0800

    sprite

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,16 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    concat: {
-      js: {
-        src: ['rectangle.js', 'calc.js'],
-        dest: 'dist/bundle.js'
+    sprite: {
+      files: {
+        src: ['images/*.png'],
+        dest: 'dist/bundle.png',
+        destCss:'dist/sprite.css'
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-spritesmith');
 
-  grunt.registerTask('default', ['concat']);
+  grunt.registerTask('default', ['sprite']);
 };
 
